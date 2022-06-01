140e5785b91764762e5fa36dd78702d9e4d80acf
Lindsey Musel <lmusel@godaddy.com>
2022-06-01 09:49:49 -0500

    Update DB host address

--- settings.py
+++ settings.py
@@ -32,7 +32,7 @@ class OTEAppConfig(AppConfig):
 
 class DevelopmentAppConfig(AppConfig):
     DB = 'devphishstory'
-    DB_HOST = '10.36.156.188'
+    DB_HOST = '10.36.190.222'
     DB_USER = 'devuser'
 
     def __init__(self):
